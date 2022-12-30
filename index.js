5156ec360a02959eed4a0c1059fe18e00adffb80
yash <yashgouravkar.gmail.com>
2022-12-30 21:45:14 +0530

    signing in users with oAuth

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+require('./services/passport');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ bye: 'there' });
-});
+require('./routes/authRoutes')(app);
 
-app.listen(5000);
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
