108509953eaedf3f01ee8865c1293a8b2dbf3f01
yash <yashgouravkar.gmail.com>
2023-01-01 16:50:44 +0530

    varibales setting up for prod

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,7 +8,13 @@ module.exports = (app) => {
     })
   );
 
-  app.get('/auth/google/callback', passport.authenticate('google'));
+  app.get(
+    '/auth/google/callback',
+    passport.authenticate('google'),
+    (req, res) => {
+      res.redirect('/surveys');
+    }
+  );
 
   app.get('/api/logout', (req, res) => {
     req.logout(); // automatically attached to the request object by passport
