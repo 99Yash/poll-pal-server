0b237dcd4cf92daf62f46461eea414a5d48718ea
yash <yashgouravkar.gmail.com>
2023-01-08 11:48:07 +0530

    added Survey schema and a subdoc

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cookieSession = require('cookie-session');
 const passport = require('passport');
 
 require('./models/User');
+require('./models/Survey');
 require('./services/passport');
 
 const app = express();
